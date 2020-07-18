121faed1c556210766f4fba6b3a57709a4b88d26
Alex Perry <alexrayperry@gmail.com>
2020-07-18 00:30:14 -0700

    adding alex files.

---
+++ Alex/project-3/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, redirect, jsonify
+from flask_pymongo import PyMongo
+import json
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+from flask import request
+
+# Mongo Flask Connection
+
+app = Flask(__name__)
+
+
+@app.route('/')
+
+def welcome():
+    return render_template('index.html')
+
+# Boiler plate 
+if __name__ == "__main__":
+    app.run(debug=True)
