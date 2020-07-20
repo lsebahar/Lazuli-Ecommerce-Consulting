3b076b162c85c5bae746851e39d5a29a7e663cf0
Thomas Wheeler <tbwheeler94@gmail.com>
2020-07-19 17:08:11 -0700

    Third Push

--- Tom/Customer-Satisfaction-WebApp/app.py
+++ Tom/Customer-Satisfaction-WebApp/app.py
@@ -14,17 +14,22 @@ app = flask.Flask(__name__, template_folder='templates')
 def main():
     if flask.request.method == 'GET':
         # Just render the initial form, to get input
-        return(flask.render_template('index.html'))
+        return(flask.render_template('main.html'))
 
     if flask.request.method == 'POST':
         # Extract the input
-        Total Order Value = flask.request.form['Total Order Value']
         product_description_lenght = flask.request.form['product_description_lenght']
-        Customer City = flask.request.form['Customer City']
+        product_photos_qty = flask.request.form['product_photos_qty']
+        Is_It_Late = flask.request.form['Is_It_Late']
+        Payment_Type = flask.request.form['Payment_Type']
+        Total_Order_Value = flask.request.form['Total_Order_Value']
+
 
         # Make DataFrame for model
-        input_variables = pd.DataFrame([[Total Order Value, product_description_lenght, Customer City]],
-                                       columns=['Total Order Value', 'product_description_lenght', 'Customer City'],
+        input_variables = pd.DataFrame([[product_description_lenght, product_photos_qty, Is_It_Late, Payment_Type,
+                                        Total_Order_Value]],
+                                       columns=['product_description_lenght', 'product_photos_qty', 'Is_It_Late',
+                                       'Payment_Type', 'Total_Order_Value'],
                                        dtype=float,
                                        index=['input'])
 
@@ -33,12 +38,14 @@ def main():
     
         # Render the form again, but add in the prediction and remind user
         # of the values they input before
-        return flask.render_template('index.html',
-                                     original_input={'Temperature':Total Order Value,
-                                                     'product_description_lenght':product_description_lenght,
-                                                     'Customer City':Customer City},
+        return flask.render_template('main.html',
+                                     original_input={'product_description_lenght':product_description_lenght,
+                                                     'product_photos_qty':product_photos_qty,
+                                                     'Is_It_Late':Is_It_Late,
+                                                     'Payment_Type':Payment_Type,
+                                                     'Total_Order_Value':Total_Order_Value},
                                      result=prediction,
                                      )
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
